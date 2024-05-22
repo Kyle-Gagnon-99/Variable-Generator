3e1b857c9140e4e549cea6dc1942ea7e3b5908c0
Kyle Gagnon <kmgagnon99@gmail.com>
2024-05-22 20:59:24 +0000

    feat: Adjusting CSS

--- css/css.py
+++ css/css.py
@@ -1,3 +1,23 @@
+import re
+
+
+def is_alias(value: str) -> bool:
+    # Check if the value is an alias by checking if it contains a color
+    # Example: "colors:primary" -> True
+    # Example: "primary" -> False
+    # Example: "Collection Name With Spaces:variable-name" -> True
+    # Example: "Just a string with a colon in it: It will fail because there is a space before the colon" -> False
+    # Example: "Just a string with a colon in it: It will fail because there is a space after the colon " -> False
+    return re.search(r"[\w\s]+:[\w\s]+", value) is not None
+
+
+def convert_alias_to_css(value: str) -> str:
+    # Split the alias into collection and variable and only return the variable
+    # Example: "colors:primary" -> "primary"
+    # Also, convert the variable to kebab-case
+    return value.split(":")[1].replace("/", "-")
+
+
 def generate_css(variables: dict, config: dict) -> str:
     """Generates CSS from the variables and config.
 
@@ -10,28 +30,38 @@ def generate_css(variables: dict, config: dict) -> str:
     """
     css_content = ""
     prefix = config["css"]["prefix"]
-    
-    def process_variables(var_dict: dict, path=""):
+
+    def process_variables(mode: str, var_dict: dict, path=""):
         nonlocal css_content
         for key, value in var_dict.items():
             current_path = f"{path}-{key}" if path else key
             if isinstance(value, dict) and 'values' in value:
-                for mode, mode_value in value['values'].items():
-                    css_var = f"{prefix}-{current_path.lower()}"
+                mode_value = value['values'].get(mode)
+                if prefix != "":
+                    css_var = f"--{prefix}-{current_path}"
+                else:
+                    css_var = f"--{current_path}"
+                # If mode_value is an alias, convert it to CSS variable
+                if isinstance(mode_value, str) and is_alias(mode_value):
+                    if prefix != "":
+                        css_content += f"\t{css_var}: var(--{prefix}-{convert_alias_to_css(mode_value)});\n"
+                    else:
+                        css_content += f"\t{css_var}: var(--{convert_alias_to_css(mode_value)});\n"
+                else:
                     css_content += f"\t{css_var}: {mode_value};\n"
             elif isinstance(value, dict):
-                process_variables(value, current_path)
-            
+                process_variables(mode, value, current_path)
+
     for collection in variables['collections']:
         modes: list[str] = collection['modes']
         if len(modes) == 1:
             css_content += f":root {{\n"
-            process_variables(collection['variables'])
+            process_variables(modes[0], collection['variables'])
             css_content += "}\n"
         else:
             for mode in modes:
                 css_content += f"[data-theme=\"{mode}\"] {{\n"
-                process_variables(collection['variables'])
+                process_variables(mode, collection['variables'])
                 css_content += "}\n\n"
-    
+
     return css_content
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from utils.generator_toml import load_config
 from utils.generator_json import load_json
 from css.css import generate_css
 
+
 def save_file(content: str, output_dir: str, file_name: str) -> None:
     """Saves the content to a file.
 
@@ -18,13 +19,14 @@ def save_file(content: str, output_dir: str, file_name: str) -> None:
     with open(f"{output_dir}/{file_name}", 'w') as file:
         file.write(content)
 
+
 def main():
     config = load_config('config.toml')
-    
+
     setup_logging(config['logging']['level'])
     logging.debug("Logging is setup.")
     logging.info("Starting the generator.")
-    
+
     logging.debug(config)
 
     # Go through each input file and get the variables/process them
@@ -33,8 +35,10 @@ def main():
         logging.info(f"Processing {full_input_file}")
         variables = load_json(full_input_file)
         css_content = generate_css(variables, config)
-        save_file(css_content, config['css']['output']['dir'], f"{input_file}.css")
+        save_file(css_content, config['css']['output']
+                  ['dir'], f"{os.path.splitext(input_file)[0]}.css")
         logging.info(f"Finished processing {input_file}")
+
 
 if __name__ == '__main__':
     main()
